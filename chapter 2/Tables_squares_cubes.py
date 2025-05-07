3c66de173eaa397bff4ee51ac257859143be5e8a
Theophilus131 <theophilusumar27@gmail.com>
2025-05-07 09:38:07 +0100

    Palindrome number

---
+++ chapter 2/Tables_squares_cubes.py
@@ -0,0 +1,29 @@
+number0 = 0
+number1=  1
+number2 = 2
+number3=  3
+number4=  4
+number5=  5
+
+square0 = number0 ** 2
+square1 = number1 ** 2
+square2 = number2 ** 2
+square3 = number3 ** 2
+square4 = number4 ** 2
+square5 = number5 ** 2
+
+cube0 = number0 ** 3
+cube1 = number1 ** 3
+cube2=  number2 ** 3
+cube3 = number3 ** 3
+cube4 = number4 ** 3
+cube5 = number5 ** 3
+
+print("number\tsquare\tcube")
+print(number0, "\t" ,square0, "\t" ,cube0)
+print(number1, "\t" ,square1, "\t" ,cube1)
+print(number2, "\t" ,square2, "\t" ,cube2)
+print(number3, "\t" ,square3, "\t" ,cube3)
+print(number4, "\t" ,square4, "\t" ,cube4)
+print(number5, "\t" ,square5, "\t" ,cube5)
+
---
+++ chapter 2/Target_heart_rate_calculator.py
@@ -0,0 +1,4 @@
+age = input(Enter your age)
+YEARS = 220
+
+maximum_heart_rate = age - 
