8707bd6273c76fa1fffe683483a9722f64137be0
Theophilus131 <theophilusumar27@gmail.com>
2025-05-07 18:12:22 +0100

    heart rate

--- chapter 2/Target_heart_rate_calculator.py
+++ chapter 2/Target_heart_rate_calculator.py
@@ -1,4 +1,11 @@
-age = input(Enter your age)
-YEARS = 220
+age = int(input("Enter your age:"))
 
-maximum_heart_rate = age - 
+maximum_heart_rate = 220 - age
+ 	
+first_target_heart_rate = maximum_heart_rate * 50 / 100
+
+second_target_heart_rate = maximum_heart_rate * 85 / 100
+
+
+print(f"The maximum heart rate is {maximum_heart_rate} and the target heart is {first_target_heart_rate} to {second_target_heart_rate}")
+	
