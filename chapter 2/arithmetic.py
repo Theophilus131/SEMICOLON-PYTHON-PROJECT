2c48be3c5721af24b02b8059e094883819c27b04
Theophilus131 <theophilusumar27@gmail.com>
2025-05-07 19:31:00 +0100

    Arithetic

---
+++ chapter 2/arithmetic.py
@@ -0,0 +1,29 @@
+left_operand = 27.5
+
+right_operand = 2
+
+
+total = left_operand + right_operand
+
+subtraction = left_operand - right_operand
+
+product = left_operand * right_operand
+
+power =  left_operand ** right_operand
+
+floor  = left_operand // right_operand
+
+division = left_operand / right_operand
+
+print(total)
+
+print(subtraction)
+
+print(product)
+
+print(power)
+
+print(floor)
+
+print(division)
+
