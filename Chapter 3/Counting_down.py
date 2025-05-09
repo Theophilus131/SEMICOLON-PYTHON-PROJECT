b710697524f0767b4880ffa5dd5f52cb8af14a68
Theophilus131 <theophilusumar27@gmail.com>
2025-05-09 04:26:57 +0100

    assignment

---
+++ Chapter 3/Counting_down.py
@@ -0,0 +1,11 @@
+
+number = int(input("enter number:"))
+
+while number < 1:
+	number = int(input("enter number"))
+
+for number in range(number, 0, -1):
+	
+	print(number)
+
+print("Blast off!")
--- Chapter 3/ammount.return.py
+++ Chapter 3/ammount.return.py
@@ -2,7 +2,7 @@ principal = int (1000)
 annual_rate = float (7 / 100)
 
 for number in range (1, 31):
-	number_year = int(input("Enter the number of years")
+	number_year = int(input("Enter the number of years: "))
 	amount_deposit =float( principal * (1 + annual_rate) ** number_year)
 
 
