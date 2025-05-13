cabdaade517489c8ceafd3f0180cff58d153a6c1
Theophilus131 <theophilusumar27@gmail.com>
2025-05-13 12:29:55 +0100

    function demo

---
+++ My Task/square_demo.py
@@ -0,0 +1,12 @@
+def get_square(number):
+	return number ** 2
+	return 
+
+print(get_square(5))
+print(get_square(15))
+print(get_square(25), get_square(12))
+
+
+def get_sum(number1, number2):
+	return number1 + number2
+print(get_sum(12, 5))
