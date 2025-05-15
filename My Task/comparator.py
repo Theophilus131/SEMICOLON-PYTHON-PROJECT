12a2c08e79a5514e48d12f03fe584214836b2d3b
Theophilus131 <theophilusumar27@gmail.com>
2025-05-15 09:24:56 +0100

    comparator

---
+++ My Task/comparator.py
@@ -0,0 +1,12 @@
+number1 = input("enter first number: ")
+
+number2 = input("enter second number: ")
+
+if number1 == number2:
+	print("0")
+
+if number1 > number2:
+	print("1")
+
+if number2 > number1:
+	print("-1")
