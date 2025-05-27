06e358a2b453808dd65ee6538aefbc354900c693
Theophilus131 <theophilusumar27@gmail.com>
2025-05-27 14:45:29 +0100

    encrypt letter

---
+++ PHASE-GATE/encrypt_text.py
@@ -0,0 +1,8 @@
+
+
+def encrypt_text(string):
+	user_input = input("Enter character: ")
+	
+	for user_input in range(1 , 14):
+		return user_input
+#print(encrypt_text)
---
+++ PHASE-GATE/test_encrypt_text.py
@@ -0,0 +1,15 @@
+import unittest
+
+import encrypt_text
+
+class TestStore(unittest.TestCase):
+
+	def test_that_to_do_list_manager_function_exist(self):
+		encrypt_text.encrypt_text()
+
+	def test_string_is_not_negative(self):
+		encrypt_text.create_character(-gfagg)
+
+
+
+	
