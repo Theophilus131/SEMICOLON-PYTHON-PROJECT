c4dba553cb4f9f13f5a248c110e3a2e45476b809
Theophilus131 <theophilusumar27@gmail.com>
2025-06-24 15:10:12 +0100

    turple

---
+++ Class- work/turple.py
@@ -0,0 +1,20 @@
+
+def unpack_collection(collection):
+    result = [0,0,0,0]
+
+    for index in range(4):
+        result[index] = collection[index]
+
+    return result
+
+my_list = [1,2,3,4,5,6,7,8,9]
+
+#print(unpack_collection(my_list))
+
+def unpacking_collection(numbers):
+
+    first_number, second_number, third_number, *others = numbers
+
+    return  first_number, second_number, third_number, others
+
+print(unpacking_collection(my_list))
