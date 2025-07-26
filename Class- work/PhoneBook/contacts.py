4979bf1e2569d8c0af3704fa10cdb78a49fbedb4
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-07-26 04:51:16 +0100

    working on phone book, adding the contacts

---
+++ Class- work/PhoneBook/contacts.py
@@ -0,0 +1,7 @@
+
+class Contacts:
+    def __init__(self, first_name, last_name, phone_number):
+        self._first_name = first_name
+        self._last_name = last_name
+        self._phone_number = phone_number
+
